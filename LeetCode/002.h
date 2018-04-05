de6ad0d0c4ddaceae84422ee2b8149ec9c19d9d8
WuJinze <294843472@qq.com>
2018-04-05 18:27:05 +0800

    002_answer

--- LeetCode/002.h
+++ LeetCode/002.h
@@ -7,42 +7,54 @@ struct ListNode {
 
 class Solution {
 public:
-	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		bool carryBit = false;
-		ListNode* head = new ListNode(0);
-		ListNode* pre = head;
-		while (l1 != NULL && l2 != NULL) {
-			ListNode* now = new ListNode(0);
-			pre->next = now;
-			pre = now;
-			now->val = (l1->val + l2->val + carryBit) % 10;
-			carryBit = (l1->val + l2->val + carryBit > 9) ? true : false;
-			l1 = l1->next; l2 = l2->next;
+	//ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+	//	bool carryBit = false;
+	//	ListNode* head = new ListNode(0);
+	//	ListNode* pre = head;
+	//	while (l1 != NULL && l2 != NULL) {
+	//		ListNode* now = new ListNode(0);
+	//		pre->next = now;
+	//		pre = now;
+	//		now->val = (l1->val + l2->val + carryBit) % 10;
+	//		carryBit = (l1->val + l2->val + carryBit > 9) ? true : false;
+	//		l1 = l1->next; l2 = l2->next;
+	//	}
+	//	if (l1 != NULL && l2 == NULL)
+	//		while (l1 != NULL)
+	//		{
+	//			ListNode* now = new ListNode((l1->val + carryBit) % 10);
+	//			carryBit = (l1->val + carryBit) > 9;
+	//			pre->next = now;
+	//			pre = now;
+	//			l1 = l1->next;
+	//		}
+	//	if (l1 == NULL && l2 != NULL)
+	//		while (l2 != NULL)
+	//		{
+	//			ListNode* now = new ListNode((l2->val + carryBit) % 10);
+	//			carryBit = (l2->val + carryBit) > 9;
+	//			pre->next = now;
+	//			pre = now;
+	//			l2= l2->next;
+	//		}
+	//	if (carryBit) {
+	//		ListNode* now = new ListNode(1);
+	//		pre->next = now;
+	//		pre = now;
+	//	}
+	//	return head->next;
+	//}
+
+	//别人的做法
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+		ListNode dummy(0), *tail = &dummy;
+		for (div_t sum{ 0, 0 }; sum.quot || l1 || l2; tail = tail->next) {
+			if (l1) { sum.quot += l1->val; l1 = l1->next; }
+			if (l2) { sum.quot += l2->val; l2 = l2->next; }
+			sum = div(sum.quot, 10);
+			tail->next = new ListNode(sum.rem);
 		}
-		if (l1 != NULL && l2 == NULL)
-			while (l1 != NULL)
-			{
-				ListNode* now = new ListNode((l1->val + carryBit) % 10);
-				carryBit = (l1->val + carryBit) > 9;
-				pre->next = now;
-				pre = now;
-				l1 = l1->next;
-			}
-		if (l1 == NULL && l2 != NULL)
-			while (l2 != NULL)
-			{
-				ListNode* now = new ListNode((l2->val + carryBit) % 10);
-				carryBit = (l2->val + carryBit) > 9;
-				pre->next = now;
-				pre = now;
-				l2= l2->next;
-			}
-		if (carryBit) {
-			ListNode* now = new ListNode(1);
-			pre->next = now;
-			pre = now;
-		}
-		return head->next;
+		return dummy.next;
 	}
 };
 
