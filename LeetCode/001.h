cc9651694c592b9b5d82af01eff305b6cbaa8865
WuJinze <294843472@qq.com>
2018-04-05 10:49:03 +0800

    001_my_answer

--- LeetCode/001.h
+++ LeetCode/001.h
@@ -6,11 +6,11 @@ using std::vector;
 class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
-		for (auto i = 0; i < nums.size(); i++)
-			for (auto j = i + 1; j < nums.size(); j++) {
+		auto size = nums.size();
+		for (auto i = 0; i < size; i++)
+			for (auto j = i + 1; j < size; j++) {
 				if (nums[i] + nums[j] == target)
 					return vector<int>{i, j};
 			}
-		return vector<int>();
 	}
 };
