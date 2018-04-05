317249c81151a512036b77f5fc24b10a5edc9ee1
WuJinze <294843472@qq.com>
2018-04-05 11:10:31 +0800

    001_answer

--- LeetCode/001.h
+++ LeetCode/001.h
@@ -1,16 +1,28 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
 
 using std::vector;
+using std::unordered_map;
 
 class Solution {
 public:
+	//Brute Force
+	//vector<int> twoSum(vector<int>& nums, int target) {
+	//	auto size = nums.size();
+	//	for (auto i = 0; i < size; i++)
+	//		for (auto j = i + 1; j < size; j++) {
+	//			if (nums[i] + nums[j] == target)
+	//				return vector<int>{i, j};
+	//		}
+	//}
+
 	vector<int> twoSum(vector<int>& nums, int target) {
-		auto size = nums.size();
-		for (auto i = 0; i < size; i++)
-			for (auto j = i + 1; j < size; j++) {
-				if (nums[i] + nums[j] == target)
-					return vector<int>{i, j};
-			}
+		unordered_map<int, int> map;
+		for (int i = 0; i != nums.size(); ++i) {
+			if (map.find(target - nums[i]) != map.end()) 
+				return{ map.find(target - nums[i])->second,i };
+			map.emplace(nums[i], i);
+		}
 	}
 };
